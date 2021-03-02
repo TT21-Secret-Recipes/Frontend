ca05dd728d4272f2d6c06d82750d314282bcb972
Matthew Ellis <mbellistx@gmail.com>
2021-03-02 00:54:53 -0600

    deleted old code

--- src/components/SignupPage.js
+++ src/components/SignupPage.js
@@ -51,110 +51,79 @@ export default function SignupPage(){
   }
 
   return (
-    // <div>
-    //   <div>
-    //     <Link to={path.login}>Login</Link>
-    //   </div>
-    //   <h1>Register</h1>
+    <DivFlexStyled>
+          
+      <DivFlexGrowStyled>
 
-    //   <form onSubmit={onSubmit}>
-    //     <label>
-    //         <input type='text' name='username' placeholder='Username...' value={values.username} onChange={onChange} />
-    //     </label>
+        <DivToggleFormStyled>
+          <LinkStyled to={path.login}>Login</LinkStyled>
+        </DivToggleFormStyled>
 
-    //     <label>
-    //       <input type='email' name='email' placeholder='Email...' value={values.email} onChange={onChange}/>
-    //     </label>
+        <H1TitleStyled>Register</H1TitleStyled>
 
-    //     <label>
-    //       <input type='text' name='password' placeholder='Password...' value={values.password} onChange={onChange}/>
-    //     </label>
+        <form onSubmit={onSubmit} id='login'>
+          
+          <DivFieldsetStyled>
+            <LabelStyled focus={focus.username} htmlFor='username'>Username</LabelStyled>
+            {/* <LabelStyled>{focus.username ? 'Username' : undefined}</LabelStyled> */}
+              <InputStyled
+                id='username'
+                type='text'
+                name='username' 
+                // placeholder={!focus.username ? 'Username...' : undefined}
+                value={values.username}
+                onChange={onChange} 
+                onFocus={onFocus} 
+                onBlur={onBlur}/>
+          </DivFieldsetStyled>
 
-    //     <label>
-    //       <input type='text' name='passwordConf' placeholder='Confirm Password...' value={values.passwordConf} onChange={onChange}/>
-    //     </label>
-
-    //     <p>{error}</p>
-
-    //     <div>
-    //       <button type='submit'>Login</button>
-    //     </div>
-    //   </form>
-    // </div>
-
-  <DivFlexStyled>
-        
-    <DivFlexGrowStyled>
-
-      <DivToggleFormStyled>
-        <LinkStyled to={path.login}>Login</LinkStyled>
-      </DivToggleFormStyled>
-
-      <H1TitleStyled>Register</H1TitleStyled>
-
-      <form onSubmit={onSubmit} id='login'>
-        
-        <DivFieldsetStyled>
-          <LabelStyled focus={focus.username} htmlFor='username'>Username</LabelStyled>
-          {/* <LabelStyled>{focus.username ? 'Username' : undefined}</LabelStyled> */}
+          <DivFieldsetStyled>
+            <LabelStyled focus={focus.email} htmlFor='email'>Email</LabelStyled>
             <InputStyled
-              id='username'
+              id='email'
               type='text'
-              name='username' 
-              // placeholder={!focus.username ? 'Username...' : undefined}
-              value={values.username}
-              onChange={onChange} 
-              onFocus={onFocus} 
+              name='email'
+              // placeholder={!focus.email ? 'email...' : undefined}
+              value={values.email}
+              onChange={onChange}
+              onFocus={onFocus}
               onBlur={onBlur}/>
-        </DivFieldsetStyled>
+          </DivFieldsetStyled>
 
-        <DivFieldsetStyled>
-          <LabelStyled focus={focus.email} htmlFor='email'>Email</LabelStyled>
-          <InputStyled
-            id='email'
-            type='text'
-            name='email'
-            // placeholder={!focus.email ? 'email...' : undefined}
-            value={values.email}
-            onChange={onChange}
-            onFocus={onFocus}
-            onBlur={onBlur}/>
-        </DivFieldsetStyled>
+          <DivFieldsetStyled>
+            <LabelStyled focus={focus.password} htmlFor='password'>Password</LabelStyled>
+            <InputStyled
+              id='password'
+              type='text'
+              name='password'
+              // placeholder={!focus.password ? 'Password...' : undefined}
+              value={values.password}
+              onChange={onChange}
+              onFocus={onFocus}
+              onBlur={onBlur}/>
+          </DivFieldsetStyled>
 
-        <DivFieldsetStyled>
-          <LabelStyled focus={focus.password} htmlFor='password'>Password</LabelStyled>
-          <InputStyled
-            id='password'
-            type='text'
-            name='password'
-            // placeholder={!focus.password ? 'Password...' : undefined}
-            value={values.password}
-            onChange={onChange}
-            onFocus={onFocus}
-            onBlur={onBlur}/>
-        </DivFieldsetStyled>
+          <DivFieldsetStyled>
+            <LabelStyled focus={focus.passwordConf} htmlFor='passwordConf'>Confirm Password</LabelStyled>
+            <InputStyled
+              id='passwordConf'
+              type='text'
+              name='passwordConf'
+              // placeholder={!focus.passwordConf ? 'PasswordConf...' : undefined}
+              value={values.passwordConf}
+              onChange={onChange}
+              onFocus={onFocus}
+              onBlur={onBlur}/>
+          </DivFieldsetStyled>
+        
+        </form>
 
-        <DivFieldsetStyled>
-          <LabelStyled focus={focus.passwordConf} htmlFor='passwordConf'>Confirm Password</LabelStyled>
-          <InputStyled
-            id='passwordConf'
-            type='text'
-            name='passwordConf'
-            // placeholder={!focus.passwordConf ? 'PasswordConf...' : undefined}
-            value={values.passwordConf}
-            onChange={onChange}
-            onFocus={onFocus}
-            onBlur={onBlur}/>
-        </DivFieldsetStyled>
-      
-      </form>
+        <p>{error}</p>
+      </DivFlexGrowStyled>
 
-      <p>{error}</p>
-    </DivFlexGrowStyled>
-
-    <DivButtonPaddingStyled>
-      <ButtonSubmitStyled type='submit' form='login'>Login</ButtonSubmitStyled>
-    </DivButtonPaddingStyled>
-  </DivFlexStyled>
+      <DivButtonPaddingStyled>
+        <ButtonSubmitStyled type='submit' form='login'>Login</ButtonSubmitStyled>
+      </DivButtonPaddingStyled>
+    </DivFlexStyled>
   )
 }
