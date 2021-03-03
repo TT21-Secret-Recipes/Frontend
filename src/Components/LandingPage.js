738b12d7c00167ccb76685f13896a7c86603a08a
Jiawei Wu <abc1929@gmail.com>
2021-03-03 08:48:17 +0800

    fix folder

---
+++ src/Components/LandingPage.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import { Route, Link, Switch } from "react-router-dom";
+import styled from "styled-components";
+import axios from "axios";
+
+import { path } from "../Routes/routes";
+import LoginPage from "./LoginPage";
+import WelcomePage from "./WelcomePage";
+import SignupPage from "./SignupPage";
+
+const DivMainStyled = styled.div`
+   border: 1px solid black;
+   border-radius: 7px;
+   left: 50%;
+   margin: 0;
+   position: absolute;
+   top: 50%;
+   transform: translate(-50%, -75%);
+   text-align: center;
+   width: 500px;
+   height: 500px;
+`;
+
+export default function LandingPage() {
+   return (
+      <DivMainStyled>
+         <Switch>
+            <Route exact path={path.welcome}>
+               <WelcomePage />
+            </Route>
+
+            <Route path={path.login}>
+               <LoginPage />
+            </Route>
+
+            <Route path={path.signup}>
+               <SignupPage />
+            </Route>
+         </Switch>
+      </DivMainStyled>
+   );
+}
---
+++ src/Components/LoginPage.js
@@ -0,0 +1,106 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import axios from 'axios';
+
+import { path } from '../Routes/routes';
+import { DivFlexStyled, DivFlexGrowStyled, DivToggleFormStyled, LinkStyled, H1TitleStyled, DivFieldsetStyled, LabelStyled, InputStyled, DivButtonPaddingStyled, ButtonSubmitStyled, PRedStyled } from './SharedStyles';
+import schema from '../yupSchema/loginSchema';
+
+const initialValues = {
+  username: '',
+  password: '',
+}
+
+const initialFocus = {
+  username: false,
+  password: false,
+}
+
+export default function LoginPage({submit}){
+  const [ values, setValues ] = useState(initialValues);
+  const [ focus, setFocus ] = useState(initialFocus);
+  const [ errors, setErrors ] = useState([]);
+
+  function onChange(evt){
+    const { name, value } = evt.target;
+    setValues({...values, [name]: value});
+  }
+
+  function onFocus(evt){
+    const { name } = evt.target;
+    setFocus({...initialFocus, [name]: true});
+  }
+
+  function onBlur(evt){
+    setFocus(initialFocus);
+  }
+
+  function onSubmit(evt){
+    evt.preventDefault();
+
+    try {
+      schema.validateSync(values, { abortEarly: false });
+      setErrors([]);
+    } catch(err) {
+      const list = err.inner.map( error => error.errors[0] );
+      setErrors(list);
+    }
+
+    axios.post('url', values)
+      .then( respones => {
+        //login user
+      })
+      .catch( err => {
+        // assuming the error looks something like "username and password do not match"
+        //setErrors(err.data);
+      });
+  }
+
+  return (
+    <DivFlexStyled>
+      
+      <DivFlexGrowStyled>
+
+        <DivToggleFormStyled>
+          <LinkStyled to={path.signup}>Register</LinkStyled>
+        </DivToggleFormStyled>
+
+        <H1TitleStyled>Login</H1TitleStyled>
+
+        <form onSubmit={onSubmit} id='login'>
+          
+          <DivFieldsetStyled>
+            <LabelStyled focus={focus.username} htmlFor='username' hasData={values.username === '' ? false : true}>Username</LabelStyled>
+              <InputStyled
+                id='username'
+                type='text'
+                name='username' 
+                value={values.username}
+                onChange={onChange} 
+                onFocus={onFocus} 
+                onBlur={onBlur}/>
+          </DivFieldsetStyled>
+
+          <DivFieldsetStyled>
+            <LabelStyled htmlFor='password' focus={focus.password} hasData={values.password === '' ? false : true}>Password</LabelStyled>
+            <InputStyled
+              id='password'
+              type='text'
+              name='password'
+              value={values.password}
+              onChange={onChange}
+              onFocus={onFocus}
+              onBlur={onBlur}/>
+          </DivFieldsetStyled>
+        
+        </form>
+        {errors.map( (error, i) => <PRedStyled key={i}>{error}</PRedStyled>)}
+      </DivFlexGrowStyled>
+
+      <DivButtonPaddingStyled>
+        <ButtonSubmitStyled type='submit' form='login'>Login</ButtonSubmitStyled>
+      </DivButtonPaddingStyled>
+    </DivFlexStyled>
+  )
+}
