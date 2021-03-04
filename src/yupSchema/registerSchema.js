719f30a1884fe6f01d3dfae5dae730b6144cbee8
Matthew Ellis <mbellistx@gmail.com>
2021-03-03 21:23:56 -0600

    updated register schema

--- src/yupSchema/registerSchema.js
+++ src/yupSchema/registerSchema.js
@@ -4,7 +4,7 @@ const loginSchema = yup.object().shape({
   username: yup.string()
     .required('Username is required'),
   email: yup.string()
-    .email()
+    .email('Please enter a valid email')
     .required('Email is required'),
   password: yup.string()
     .required('Password is required'),
